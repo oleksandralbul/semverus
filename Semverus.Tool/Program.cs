bef65a18dc7a5a4be492b596d7dc12580dfbce43
Oleksandr <oleksandr.albul@outlook.com>
2025-04-30 10:49:56 +0300

    Add solution and tool project

---
+++ Semverus.Tool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Semverus.Tool;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var version = Assembly
+			.GetEntryAssembly()?
+			.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+			.InformationalVersion;
+
+		Console.WriteLine($"Semverus v{version}");
+	}
+}
