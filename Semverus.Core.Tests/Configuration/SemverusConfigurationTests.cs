6b351e1b4982c8f7f514a7606b8c41d41a09eab5
Oleksandr <oleksandr.albul@outlook.com>
2025-05-13 17:43:09 +0300

    add tests for SemverusConfiguration

---
+++ Semverus.Core.Tests/Configuration/SemverusConfigurationTests.cs
@@ -0,0 +1,92 @@
+﻿using Semverus.Core.Configuration;
+
+namespace Semverus.Core.Tests.Configuration;
+
+public class SemverusConfigurationTests
+{
+	[Fact]
+	public void SemverusConfiguration_AllFieldsAreNull()
+	{
+		var config = new SemverusConfiguration();
+
+		foreach (var property in typeof(SemverusConfiguration).GetProperties())
+			Assert.Null(property.GetValue(config));
+	}
+
+	[Fact]
+	public void SemverusConfiguration_SetPropertyDuringInitialization()
+	{
+		var expectedPropertyValue = "myValue";
+		var config = new SemverusConfiguration()
+		{
+			AssemblyInformationalVersionFormat = expectedPropertyValue
+		};
+
+		Assert.Equal(expectedPropertyValue, config.AssemblyInformationalVersionFormat);
+	}
+
+	[Fact]
+	public void Merge_OtherIsNull_NoChanges()
+	{
+		var expectedPropertyValue = "myValue";
+		var config = new SemverusConfiguration()
+		{
+			AssemblyInformationalVersionFormat = expectedPropertyValue
+		};
+
+		config.Merge(null!);
+
+		Assert.Equal(expectedPropertyValue, config.AssemblyInformationalVersionFormat);
+	}
+
+	[Fact]
+	public void Merge_OtherProperyValueNull_NoChanges()
+	{
+		var expectedPropertyValue = "myValue";
+		var config = new SemverusConfiguration()
+		{
+			AssemblyInformationalVersionFormat = expectedPropertyValue
+		};
+
+		var toMerge = new SemverusConfiguration();
+
+		config.Merge(toMerge);
+
+		Assert.Equal(expectedPropertyValue, config.AssemblyInformationalVersionFormat);
+	}
+
+	[Fact]
+	public void Merge_OtherProperyDifferent_ExpectChanges()
+	{
+		var initialPropertyValue = "firstValue";
+		var expectedPropertyValue = "newValue";
+		var config = new SemverusConfiguration()
+		{
+			AssemblyInformationalVersionFormat = initialPropertyValue
+		};
+
+		var toMerge = new SemverusConfiguration()
+		{
+			AssemblyInformationalVersionFormat = expectedPropertyValue
+		};
+
+		config.Merge(toMerge);
+
+		Assert.Equal(expectedPropertyValue, config.AssemblyInformationalVersionFormat);
+	}
+
+	[Fact]
+	public void ToString_OutputCorrect()
+	{
+		var propertyValue = "myValue";
+		var expectedString = "AssemblyInformationalVersionFormat: myValue" + Environment.NewLine;
+
+		var config = new SemverusConfiguration()
+		{
+			AssemblyInformationalVersionFormat = propertyValue
+		};
+
+		var toString = config.ToString();
+		Assert.Equal(expectedString, toString);
+	}
+}
